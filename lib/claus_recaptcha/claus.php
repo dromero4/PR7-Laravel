8546ec3e59aa716e7b0bb917fdff52ad581e017a
David Romero <147045613+dromero4@users.noreply.github.com>
2025-04-10 15:14:45 +0200

    Delete lib/claus_recaptcha directory

--- lib/claus_recaptcha/claus.php
+++
@@ -1,7 +0,0 @@
-<?php
-//$clau_publica = "6LdCa5AqAAAAAF9jnZt4W7Ebat4e7D98J3rX0FlL";
-//$clau_privada = "6LdCa5AqAAAAAKLlRVgKX9c7sfVWgotCq9mhZl8X";
-return [
-    'site_key' => '6LdCa5AqAAAAAF9jnZt4W7Ebat4e7D98J3rX0FlL',
-    'secret_key' => '6LdCa5AqAAAAAKLlRVgKX9c7sfVWgotCq9mhZl8X',
-];
